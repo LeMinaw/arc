4ff98f69b8d98e3b6227cb2fc0636e6d726c7435
LeMinaw <leminaw@gmail.com>
2024-01-25 21:45:41 +0100

    Refactor arc http client

--- arc-client/src/business/arc.js
+++ arc-client/src/business/arc.js
@@ -1,51 +1,64 @@
 const BASE_URL = "http://127.0.0.1:8000/api/v1/"
 
-export const fetchLastProducts = async () => {
-  return await fetch(`${BASE_URL}stock/`).then(r => r.json())
-}
-
-export const fetchItem = async id => {
-  return await fetch(`${BASE_URL}items/${id}/`).then(r => r.json())
-}
-
-export const patchItem = async (id, data) => {
-  return await fetch(`${BASE_URL}items/${id}/`, {
-    method: "PATCH",
-    body: JSON.stringify(data),
+class ArcHttpClient {
+  #options = {
     headers: { "Content-type": "application/json; charset=UTF-8" },
-  }).then(r => r.json())
+  }
+
+  #cb = r => r.json()
+
+  constructor(base_url) {
+    this.root = base_url
+  }
+
+  async get(path, cb = this.#cb) {
+    return await fetch(this.root + path).then(cb)
+  }
+
+  async post(path, cb = this.#cb) {
+    return await fetch(this.root + path, {
+      method: "POST",
+      body: JSON.stringify(data),
+      ...this.#options,
+    }).then(cb)
+  }
+
+  async patch(path, data, cb = this.#cb) {
+    return await fetch(this.root + path, {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      ...this.#options,
+    }).then(cb)
+  }
+
+  async delete(path, cb = this.#cb) {
+    return await fetch(this.root + path, {
+      method: "DELETE",
+    }).then(cb)
+  }
 }
 
-export const deleteItem = async id => {
-  return await fetch(`${BASE_URL}items/${id}/`, { method: "DELETE" }).then(r =>
-    r.json()
-  )
-}
+const client = new ArcHttpClient(BASE_URL)
 
-export const fetchProduct = async id => {
-  return await fetch(`${BASE_URL}stock/${id}/`).then(r => r.json())
-}
+export const fetchItem = async id => await client.get(`items/${id}/`)
 
-export const fetchProducts = async (kindId = null) => {
-  if (kindId === null) return await fetch(`${BASE_URL}stock/`).then(r => r.json())
+export const patchItem = async (id, data) => await client.patch(`items/${id}/`, data)
 
-  return await fetch(`${BASE_URL}stock/?kind=${kindId}`).then(r =>
-    r.status === 400 ? [] : r.json()
-  )
-}
+export const deleteItem = async id => await client.delete(`items/${id}/`)
 
-export const fetchCategory = async slug => {
-  return await fetch(`${BASE_URL}categories/?slug=${slug}`)
-    .then(r => r.json())
-    .then(r => r.at(0))
-}
+export const fetchProducts = async (kindId = null) =>
+  kindId !== null
+    ? await client.get(`stock/?kind=${kindId}`, r => (r.status !== 400 ? r.json() : []))
+    : await client.get(`stock/`)
 
-export const fetchRootCategories = async () => {
-  return await fetch(`${BASE_URL}categories/?root=true`).then(r => r.json())
-}
+export const fetchLastProducts = async () => await client.get("stock/")
 
-export const fetchChildCategories = async id => {
-  return await fetch(`${BASE_URL}categories/?parent=${id}`).then(r =>
-    r.status === 400 ? [] : r.json()
-  )
-}
+export const fetchProduct = async id => await client.get(`stock/${id}/`)
+
+export const fetchCategory = async slug =>
+  await client.get(`categories/?slug=${slug}`).then(r => r.at(0))
+
+export const fetchRootCategories = async () => await client.get(`categories/?root=true`)
+
+export const fetchChildCategories = async id =>
+  await client.get(`categories/?parent=${id}`, r => (r.status !== 400 ? r.json() : []))
